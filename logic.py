34220351ca2b9a10121cb1d75db2365e4d64de75
OliverNeven <oliver@neven.dk>
2016-06-19 18:05:06 +0200

    Added blocks

--- logic.py
+++ logic.py
@@ -10,8 +10,8 @@ Example use:
 
 '''
 
-import argparse, re, copy
-from tokens import tokens
+import argparse, re, copy, time
+from tokens import tokens, TokenData
 
 # Parses and returns launch arguments
 def setup_argparse():
@@ -24,45 +24,89 @@ def get_file(pathname):
     try:
         return open(pathname, "r")
     except Exception as e:
-        print ("ERROR: Failed to open the file", pathname, "\nReason:", e)
+        print ("ERROR <File>: Failed to open the file", pathname, "\nReason:", e)
         exit (404)
 
 # Creates a list of tokens from the string for parsing
 def tokenize_text(raw_text):
+    print ("Tokenizing <", raw_text, "> ...")
     block = []
+    nested_index = 0
     raw_chunk = ""
     for char in raw_text:
         raw_chunk += char
-        print ("'", raw_chunk, "'")
+        print("'", raw_chunk, "'")
+
+        if char == "{":
+            nested_index += 1
+            print ("Nested index -->", nested_index)
+        elif char == "}":
+            nested_index -= 1
+            print("Nested index -->", nested_index)
+        if nested_index < 0:
+            print ("ERROR <Tokenizer>: Two many block endings")
+
         if len(raw_chunk.strip()) > 5 and raw_chunk.strip()[-5:] == "<EOF>":
-            print ("ERROR: Unknown token before end of file")
+            print ("ERROR <Tokenizer>: Unknown token before end of file")
             exit(404)
 
         for token in tokens:
             if (token.USES_REGEX and re.match(token.IDENTIFIER, raw_chunk.strip()) is not None) or (not token.USES_REGEX and raw_chunk.strip() == token.IDENTIFIER):
 
-                print ("Found a", token.SHOW_NAME, "token!")
-                token = copy.copy(token)
+                token_data = TokenData(token)
 
-                if token.is_block():
-                    block = tokenize_text(raw_chunk[1:-1])
-                    token.block += block
+                if token_data.TOKEN.is_block():
+                    if nested_index is not 0:
+                        break
+                    print (" -- BLOCK START --")
+                    nested_block = tokenize_text(raw_chunk.strip()[1:-1])
+                    print (" -- BLOCK END --")
+                    token_data.block += nested_block
 
-                if token.EXPECTED_ARGUMENTS is not None:
+                print ("Found a <" + token_data.TOKEN.SHOW_NAME + "> token!")
+
+                if token_data.TOKEN.EXPECTED_ARGUMENTS is not None:
                     pass
                 else:
-                    block.append(token)
+                    block.append(token_data)
                 raw_chunk = ""
     return block
-
-
 
 args = setup_argparse()
 file = get_file(args.file)
 block = tokenize_text(re.sub(r"\s+", " ", file.read().replace("\n", "<EOL>"))+"<EOL><EOF>")
 print ("\nThe following tokens were found:")
-for token in block:
-    print (token.to_string())
+for token_data in block:
+    print (token_data.to_string())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- tokens.py
+++ tokens.py
@@ -4,15 +4,8 @@ The file containing all the tokens used by the LOGIC interpreter.
 
 '''
 
+# Token object for hardcoded values
 class Token:
-    SHOW_NAME = None
-    IDENTIFIER = None
-    USES_REGEX = False
-    EXEC = None
-    EXPECTED_ARGUMENTS = None
-    block = []
-    arguments = []
-
     def __init__(self, show_name, identifier, uses_regex, function, expected_arguments):
         self.SHOW_NAME = show_name
         self.IDENTIFIER = identifier
@@ -21,29 +14,41 @@ class Token:
         self.EXPECTED_ARGUMENTS = expected_arguments
 
     def to_string(self):
-        string = "<" + self.SHOW_NAME + " "
+        return "<" + self.SHOW_NAME + ">"
 
+    def is_block(self):
+        return self.SHOW_NAME == "BLOCK"
+
+    def is_tag(self):
+        return self.IDENTIFIER == "<EOF>" or self.IDENTIFIER == "<EOL>"
+
+# Token data object for wrapping the Token object with dynamic data
+class TokenData:
+    def __init__(self, token):
+        self.TOKEN = token
+        self.arguments = []
+        self.block = []
+
+    def to_string(self):
+        if self.TOKEN.is_tag():
+            return ""
+
+        string = "{<" + self.TOKEN.SHOW_NAME + " "
         if len(self.arguments) > 0:
             string += "- "
             for argument in self.arguments:
                 string += argument + " "
         if len(self.block) > 0:
-            string += "- { "
-            for token in self.block:
-                string += token.SHOW_NAME + " "
-            string += "} "
-
+            string += "- {"
+            for token_data in self.block:
+                string += "" + token_data.to_string()
+            string += "}"
         return string.strip() + ">"
 
-    def is_block(self):
-        return self.SHOW_NAME == "BLOCK"
-
-
 tokens = [
+    Token("ECHO", "ECHO", False, )
     Token("BLOCK", "{?(\s|\S)*}", True, None, None),
-
     Token("LINE_COMMENT", "#.*<EOL>", True, None, None),
-
     Token("END_OF_LINE", "<EOL>", False, None, None)
 ]
 
